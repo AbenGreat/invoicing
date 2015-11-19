8c8b487519ffe821234ebaaa49cb1701092cc23e
fctao <fctao120@sina.cn>
2015-11-19 09:34:14 +0800

    a

--- application/views/goods/result.php
+++ application/views/goods/result.php
@@ -6,7 +6,8 @@
                                             <th>商品ID</th>
                                             <th>商品名称</th>
                                             <th>商品类型</th>
-                                            <th>商品价格</th>
+                                            <th>商品计划购入价格</th>
+											<th>商品计划出售价格</th>
 											<th>备注</th>
 											 <th>操作</th>
                                         </tr>
@@ -17,7 +18,8 @@
                                             <td><?= $data->id?></td>
                                             <td><?= $data->name?></td>
                                             <td><?= $data->type?></td>
-                                            <td><?= $data->price?></td>
+                                            <td><?= $data->coast_price?></td
+                                            <td><?= $data->sale_price?></td>
 											<td><?= $data->remarks?></td>
 											<td><a name="edit" class = "btn" data-id="<?= $data->id?>">编辑</a><a class = "btn" name="remove" data-id="<?= $data->id?>"> 删除</a></td>
                                         </tr>
--- database/database.sql
+++ database/database.sql
@@ -38,14 +38,15 @@ CREATE TABLE `goods` (
   `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '货物编码',
   `name` varchar(20) NOT NULL COMMENT '货物名称',
   `type` varchar(20) DEFAULT NULL COMMENT '货物类型',
-  `price` int(11) DEFAULT NULL COMMENT '货物价格',
+  `coast_price` int(11) DEFAULT NULL COMMENT '购入价格',
+  `sale_price` int(11) DEFAULT NULL COMMENT '出售价格',
   `remarks` varchar(50) DEFAULT NULL COMMENT '备注',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
 /*Data for the table `goods` */
 
-insert  into `goods`(`id`,`name`,`type`,`price`,`remarks`) values (1,'iphone9','数码',9999,'概念机');
+insert  into `goods`(`id`,`name`,`type`,`coast_price`,`sale_price`,`remarks`) values (1,'iphone9','数码',6666,9999,'概念机');
 
 /*Table structure for table `purchase_in` */
 
@@ -53,35 +54,85 @@ DROP TABLE IF EXISTS `purchase_in`;
 
 CREATE TABLE `purchase_in` (
   `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '采购入库单编码',
-  `supplier` varchar(20) DEFAULT NULL COMMENT '供应商名称',
-  `goods` varchar(20) DEFAULT NULL COMMENT '货物名称',
-  `number` int(11) DEFAULT NULL COMMENT '货物数量',
-  `price` int(11) DEFAULT NULL COMMENT '总价格',
-  `user_name` varchar(20) DEFAULT NULL COMMENT '操作员',
+  `supplier_id` int(11) DEFAULT NULL COMMENT '供应商ID',
+  `warehouse_id` int(11) DEFAULT NULL COMMENT '入库仓库ID',
+  `user_id` int(11) DEFAULT NULL COMMENT '操作员ID',
   `status` enum('wait_check','check_faile','wait_finish','finish_faile','finish') DEFAULT NULL COMMENT '订单状态',
   `created` timestamp NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-  PRIMARY KEY (`id`)
+  `finished` timestamp NULL DEFAULT NULL COMMENT '完成时间',
+  PRIMARY KEY (`id`),
+  KEY `supplier_id` (`supplier_id`),
+  KEY `warehouse_id` (`warehouse_id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `purchase_in_ibfk_1` FOREIGN KEY (`supplier_id`) REFERENCES `supplier` (`id`),
+  CONSTRAINT `purchase_in_ibfk_2` FOREIGN KEY (`warehouse_id`) REFERENCES `warehouse` (`id`),
+  CONSTRAINT `purchase_in_ibfk_3` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 /*Data for the table `purchase_in` */
+
+/*Table structure for table `purchase_in_details` */
+
+DROP TABLE IF EXISTS `purchase_in_details`;
+
+CREATE TABLE `purchase_in_details` (
+  `id` int(11) NOT NULL COMMENT '采购入库明细ID',
+  `parchase_in_id` int(11) DEFAULT NULL COMMENT '采购入库单ID',
+  `goods_id` int(11) DEFAULT NULL COMMENT '商品ID',
+  `number` int(11) DEFAULT NULL COMMENT '商品数量',
+  `price` float(8,2) DEFAULT NULL COMMENT '商品价格',
+  `count` float(8,2) DEFAULT NULL COMMENT '总价格',
+  PRIMARY KEY (`id`),
+  KEY `parchase_in_id` (`parchase_in_id`),
+  KEY `goods_id` (`goods_id`),
+  CONSTRAINT `purchase_in_details_ibfk_1` FOREIGN KEY (`parchase_in_id`) REFERENCES `purchase_in` (`id`),
+  CONSTRAINT `purchase_in_details_ibfk_2` FOREIGN KEY (`goods_id`) REFERENCES `goods` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `purchase_in_details` */
 
 /*Table structure for table `purchase_return` */
 
 DROP TABLE IF EXISTS `purchase_return`;
 
 CREATE TABLE `purchase_return` (
-  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '采购退货单编码',
-  `supplier` varchar(20) DEFAULT NULL COMMENT '供应商名称',
-  `goods` varchar(20) DEFAULT NULL COMMENT '货物名称',
-  `number` int(11) DEFAULT NULL COMMENT '货物数量',
-  `price` int(11) DEFAULT NULL COMMENT '总价格',
-  `user_name` varchar(20) DEFAULT NULL COMMENT '操作员',
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '采购退货单ID',
+  `supplier_id` int(11) DEFAULT NULL COMMENT '供应商ID',
+  `warehouse_id` int(11) DEFAULT NULL COMMENT '退货仓库ID',
+  `user_id` int(11) DEFAULT NULL COMMENT '操作员ID',
   `status` enum('wait_check','check_faile','wait_finish','finish_faile','finish') DEFAULT NULL COMMENT '订单状态',
   `created` timestamp NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-  PRIMARY KEY (`id`)
+  `finished` timestamp NULL DEFAULT NULL COMMENT '完成时间',
+  PRIMARY KEY (`id`),
+  KEY `supplier_id` (`supplier_id`),
+  KEY `warehouse_id` (`warehouse_id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `purchase_return_ibfk_1` FOREIGN KEY (`supplier_id`) REFERENCES `supplier` (`id`),
+  CONSTRAINT `purchase_return_ibfk_2` FOREIGN KEY (`warehouse_id`) REFERENCES `warehouse` (`id`),
+  CONSTRAINT `purchase_return_ibfk_3` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 /*Data for the table `purchase_return` */
+
+/*Table structure for table `purchase_return_details` */
+
+DROP TABLE IF EXISTS `purchase_return_details`;
+
+CREATE TABLE `purchase_return_details` (
+  `id` int(11) NOT NULL COMMENT '采购退货明细ID',
+  `parchase_return_id` int(11) DEFAULT NULL COMMENT '采购退货单ID',
+  `goods_id` int(11) DEFAULT NULL COMMENT '商品ID',
+  `number` int(11) DEFAULT NULL COMMENT '商品数量',
+  `price` float(8,2) DEFAULT NULL COMMENT '商品价格',
+  `count` float(8,2) DEFAULT NULL COMMENT '总价格',
+  PRIMARY KEY (`id`),
+  KEY `parchase_return_id` (`parchase_return_id`),
+  KEY `goods_id` (`goods_id`),
+  CONSTRAINT `purchase_return_details_ibfk_1` FOREIGN KEY (`parchase_return_id`) REFERENCES `purchase_return` (`id`),
+  CONSTRAINT `purchase_return_details_ibfk_2` FOREIGN KEY (`goods_id`) REFERENCES `goods` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `purchase_return_details` */
 
 /*Table structure for table `role` */
 
@@ -103,17 +154,42 @@ DROP TABLE IF EXISTS `sale_out`;
 
 CREATE TABLE `sale_out` (
   `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '销售出库单编码',
-  `customer` varchar(20) DEFAULT NULL COMMENT '客户名称',
-  `goods` varchar(20) DEFAULT NULL COMMENT '货物名称',
-  `number` int(11) DEFAULT NULL COMMENT '货物数量',
-  `price` int(11) DEFAULT NULL COMMENT '总价格',
-  `user_name` varchar(20) DEFAULT NULL COMMENT '操作员',
+  `customer_id` int(11) DEFAULT NULL COMMENT '客户ID',
+  `warehouse_id` int(11) DEFAULT NULL COMMENT '出库仓库ID',
+  `user_id` int(11) DEFAULT NULL COMMENT '操作员ID',
   `status` enum('wait_check','check_faile','wait_finish','finish_faile','finish') DEFAULT NULL COMMENT '订单状态',
   `created` timestamp NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-  PRIMARY KEY (`id`)
+  `finished` timestamp NULL DEFAULT NULL COMMENT '完成时间',
+  PRIMARY KEY (`id`),
+  KEY `customer_id` (`customer_id`),
+  KEY `warehouse_id` (`warehouse_id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `sale_out_ibfk_1` FOREIGN KEY (`customer_id`) REFERENCES `customer` (`id`),
+  CONSTRAINT `sale_out_ibfk_2` FOREIGN KEY (`warehouse_id`) REFERENCES `warehouse` (`id`),
+  CONSTRAINT `sale_out_ibfk_3` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 /*Data for the table `sale_out` */
+
+/*Table structure for table `sale_out_details` */
+
+DROP TABLE IF EXISTS `sale_out_details`;
+
+CREATE TABLE `sale_out_details` (
+  `id` int(11) NOT NULL COMMENT '销售出库明细ID',
+  `sale_out_id` int(11) DEFAULT NULL COMMENT '销售出库单ID',
+  `goods_id` int(11) DEFAULT NULL COMMENT '商品ID',
+  `number` int(11) DEFAULT NULL COMMENT '商品数量',
+  `price` float(8,2) DEFAULT NULL COMMENT '商品价格',
+  `count` float(8,2) DEFAULT NULL COMMENT '总价格',
+  PRIMARY KEY (`id`),
+  KEY `sale_out_id` (`sale_out_id`),
+  KEY `goods_id` (`goods_id`),
+  CONSTRAINT `sale_out_details_ibfk_1` FOREIGN KEY (`sale_out_id`) REFERENCES `sale_out` (`id`),
+  CONSTRAINT `sale_out_details_ibfk_2` FOREIGN KEY (`goods_id`) REFERENCES `goods` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `sale_out_details` */
 
 /*Table structure for table `sale_return` */
 
@@ -121,17 +197,42 @@ DROP TABLE IF EXISTS `sale_return`;
 
 CREATE TABLE `sale_return` (
   `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '销售退货单编码',
-  `customer` varchar(20) DEFAULT NULL COMMENT '客户名称',
-  `goods` varchar(20) DEFAULT NULL COMMENT '货物名称',
-  `number` int(11) DEFAULT NULL COMMENT '货物数量',
-  `price` int(11) DEFAULT NULL COMMENT '总价格',
-  `user_name` varchar(20) DEFAULT NULL COMMENT '操作员',
+  `customer_id` int(11) DEFAULT NULL COMMENT '客户ID',
+  `warehouse_id` int(11) DEFAULT NULL COMMENT '入库仓库ID',
+  `user_id` int(11) DEFAULT NULL COMMENT '操作员ID',
   `status` enum('wait_check','check_faile','wait_finish','finish_faile','finish') DEFAULT NULL COMMENT '订单状态',
   `created` timestamp NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-  PRIMARY KEY (`id`)
+  `finished` timestamp NULL DEFAULT NULL COMMENT '完成时间',
+  PRIMARY KEY (`id`),
+  KEY `customer_id` (`customer_id`),
+  KEY `warehouse_id` (`warehouse_id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `sale_return_ibfk_1` FOREIGN KEY (`customer_id`) REFERENCES `customer` (`id`),
+  CONSTRAINT `sale_return_ibfk_2` FOREIGN KEY (`warehouse_id`) REFERENCES `warehouse` (`id`),
+  CONSTRAINT `sale_return_ibfk_3` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 /*Data for the table `sale_return` */
+
+/*Table structure for table `sale_return_details` */
+
+DROP TABLE IF EXISTS `sale_return_details`;
+
+CREATE TABLE `sale_return_details` (
+  `id` int(11) NOT NULL COMMENT '销售退货明细ID',
+  `sale_return_id` int(11) DEFAULT NULL COMMENT '销售退货单ID',
+  `goods_id` int(11) DEFAULT NULL COMMENT '商品ID',
+  `number` int(11) DEFAULT NULL COMMENT '商品数量',
+  `price` float(8,2) DEFAULT NULL COMMENT '商品价格',
+  `count` float(8,2) DEFAULT NULL COMMENT '总价格',
+  PRIMARY KEY (`id`),
+  KEY `sale_return_id` (`sale_return_id`),
+  KEY `goods_id` (`goods_id`),
+  CONSTRAINT `sale_return_details_ibfk_1` FOREIGN KEY (`sale_return_id`) REFERENCES `sale_return` (`id`),
+  CONSTRAINT `sale_return_details_ibfk_2` FOREIGN KEY (`goods_id`) REFERENCES `goods` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `sale_return_details` */
 
 /*Table structure for table `stock` */
 
